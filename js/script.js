8fb305d8466cd0c57d3fe98176ee694f36ddd762
AleReyes <20210772@ricaldone.edu.sv>
2025-06-10 15:30:09 -0600

    CRUD Completo

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ async function obtenerPersona() {
 }
 
 //La función lleva un parámetro "daros" que representa al JSON
-
 function mostrarDatos(datos){
     //Se llama al tbody dentro del elemento con id "tabla"
     const tabla = document.querySelector('#tabla tbody')
@@ -29,7 +28,7 @@ function mostrarDatos(datos){
                 <td>${persona.edad}</td>
                 <td>${persona.email}</td>
                 <td>
-                    <button>Editar</button>
+                    <button onClick ="AbrirModalEditar('${persona.id}', '${persona.Nombre}', '${persona.apellido}', '${persona.email}', '${persona.edad}')">Editar</button>
                     <button onClick="EliminarPersona(${persona.id})">Eliminar</button>
                 </td>
             </tr>
@@ -99,7 +98,6 @@ document.getElementById("frmAgregar").addEventListener("submit", async e =>{
 });
 
 
-
 //Función para borrar registros
 async function EliminarPersona(id){
     const confirmacion = confirm("¿Estas seguro de eliminar este registro?");
@@ -112,3 +110,55 @@ async function EliminarPersona(id){
         obtenerPersona();
     }
 }
+
+//Proceso para editar un registro
+const modalEditar = document.getElementById("modal-editar");
+const btnCerrarEditar = document.getElementById("btnCerrarEditar");
+//Boton para cerrar
+btnCerrarEditar.addEventListener("click", () => {
+    modalEditar.close();//Cerrar Modal de Editar
+});
+
+//Boton para abrir
+function AbrirModalEditar(id, nombre, apellido, email,edad){
+    //Se agregan los valores del registro en los input
+    document.getElementById("idEditar").value = id;
+    document.getElementById("nombreEditar").value = nombre;
+    document.getElementById("apellidoEditar").value = apellido;
+    document.getElementById("edadEditar").value = edad;
+    document.getElementById("emailEditar").value = email;
+
+    modalEditar.showModal();//Modal se abre despues de agregar los valores a los input
+}
+
+document.getElementById("frmEditar").addEventListener("submit", async e=>{
+    e.preventDefault(); //Evita que el formulario se envíe
+    
+    const id = document.getElementById("idEditar").value;
+    const nombre = document.getElementById("nombreEditar").value.trim();
+    const apellido = document.getElementById("apellidoEditar").value.trim();
+    const edad = document.getElementById("edadEditar").value.trim();
+    const email = document.getElementById("emailEditar").value.trim();
+
+    if(!id || !nombre || !apellido || !edad || !email){
+        alert("Complete todos los campos");
+        return; //Evita que el código se siga ejecutando
+    }
+
+    //Llamada a la API
+    const respuesta = await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({edad, email, nombre, apellido})
+    });
+
+    if(respuesta.ok){
+        alert("Registro actualizado con éxito"); //Confirmación
+        modalEditar.close(); //Cerramos el modal
+        obtenerPersona(); //Actualizamos la lista
+    }
+    else{
+        alert("Hubo un error al actualizar");
+    }
+
+});
